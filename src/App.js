2feeb03da7d5a26b0eb2d563a706ba0ee972d3f3
Jônatas Cassiano Gouveia de Araújo <Jc_px@hotmail.com>
2023-12-07 17:14:55 -0300

    Criando react e iniciando
    configurações basicas e front page iniciado

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import fotoLogo from './foto-front-page-2.png';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <img src={fotoLogo} className="App-logo" alt="logo" />
+        <p>
+          Seja Bem-vindo ao Prefiro o da Minha Mãe!
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
